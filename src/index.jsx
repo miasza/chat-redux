a29589ad46c861750be5720cfdf84298bf50e599
Mia Szarvas <mia.szarvas@gmail.com>
2019-07-01 17:43:59 +0300

    start reducers off with identity recucer and add redux-logger and redux-promise

--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, combineReducers } from 'redux';
+import logger from 'redux-logger';
+import reduxPromise from 'redux-promise';
 
 // internal modules
 import App from './components/app';
@@ -26,7 +28,7 @@ const reducers = combineReducers({
   messages: messagesReducer,
   currentUser: identityReducer,
   selectedChannel: selectedChannelsReducer,
-  channels: channelsReducer;
+  channels: channelsReducer
 });
 
 // render an instance of the component in the DOM
