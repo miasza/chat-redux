f5791f76629d0024979e24b58835017d88dceb3d
Mia Szarvas <mia.szarvas@gmail.com>
2019-07-01 17:38:35 +0300

    create initial state, create reducers, import them to index and combine them to create reducers constant

--- src/index.jsx
+++ src/index.jsx
@@ -7,10 +7,26 @@ import { createStore, combineReducers } from 'redux';
 // internal modules
 import App from './components/app';
 import '../assets/stylesheets/application.scss';
+import messagesReducer from './reducers/messages_reducer';
+import channelsReducer from './reducers/channels_reducer';
+import identityReducer from './reducers/current_user_reducer';
+import selectedChannelsReducer from './reducers/selected_channels_reducer';
+
 
 // State and reducers
+
+const initialState = {
+  messages: [],
+  channels: [ 'general', 'react', 'paris' ],
+  currentUser: prompt("What is your username?") || `anonymous${Math.floor(10 + (Math.random() * 90))}`,
+  selectedChannel: 'general'
+};
+
 const reducers = combineReducers({
-  changeMe: (state = null, action) => state
+  messages: messagesReducer,
+  currentUser: identityReducer,
+  selectedChannel: selectedChannelsReducer,
+  channels: channelsReducer;
 });
 
 // render an instance of the component in the DOM
